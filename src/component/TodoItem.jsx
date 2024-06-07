687953abf18e85cc56eb02388a1715d6449ae22b
kanzul <muhammad.mf585@gmail.com>
2024-06-07 21:42:49 +0700

    add delete feature

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const TodoItem = ({ todo, toggleCompleted }) => {
+const TodoItem = ({ todo, toggleCompleted, deleteTodo }) => {
     const getTodoTitleStyle = () => {
         if (todo.completed === true) {
-            return { textDecoration: 'line-through' }
+            return { textDecoration: "line-through" };
         } else {
-            return { textDecoration: 'none' }
+            return { textDecoration: "none" };
         }
-    }
+    };
 
     return (
         <div style={styles.todoItem}>
@@ -17,25 +17,36 @@ const TodoItem = ({ todo, toggleCompleted }) => {
                 onChange={() => toggleCompleted(todo.id)}
             />
             <p style={getTodoTitleStyle()}>{todo.title}</p>
+            <button onClick={() => deleteTodo(todo.id)} style={styles.button}>
+                x
+            </button>
         </div>
     )
-}
+};
 
 const styles = {
     todoItem: {
         border: '2px solid #f4f4f4',
         fontSize: '24px',
-        // Tambahkan styles di bawah ini
         display: 'flex',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        padding: '0 20px',
     },
-    // Tambahkan styles di bawah ini
     checkbox: {
-        marginRight: '10px',
         height: '18px',
         width: '18px',
     },
+    button: {
+        backgroundColor: '#BB0000',
+        color: '#fff',
+        height: '30px',
+        width: '30px',
+        borderRadius: '100%',
+        border: 'none',
+        cursor: 'pointer',
+        fontSize: '16px',
+    },
 }
 
-export default TodoItem
+export default TodoItem;
